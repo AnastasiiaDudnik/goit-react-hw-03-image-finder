00c2f16dbb66ab987fcad7dd32deeb1ef5a55a90
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-03-08 21:54:25 +0200

    add modal component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Modal } from './Modal/Modal';
+
+export class App extends Component {
+  state = {
+    modalOpen: false,
+  };
+
+  toggleModal = () => {
+    this.setState(({ modalOpen }) => ({
+      modalOpen: !modalOpen,
+    }));
+  };
+
+  render() {
+    const { modalOpen } = this.state;
+
+    return (
+      <div>
+        <button type="button" onClick={this.toggleModal}>
+          Open Modal
+        </button>
+        {modalOpen && (
+          <Modal onClose={this.toggleModal}>
+            <button type="button" onClick={this.toggleModal}>
+              Close modal
+            </button>
+          </Modal>
+        )}
+      </div>
+    );
+  }
+}
